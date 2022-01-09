4324adfbecf84dc2134fecf8ca2205e1bf1144d1
Mehtap Ugur <yaziliminval@hotmail.com>
2022-01-09 20:08:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
 const methodOverride = require("method-override");
-const fs = require("fs");
 const ejs = require("ejs");
-const Photo = require("./models/Photo");
+const photoController = require("./controllers/photoControllers");
+const pageController = require("./controllers/pageController");
+
 //oluşturduğumuz app değişkenine express fonksiyonunu atarız
 const app = express();
 
@@ -12,7 +13,6 @@ const app = express();
 mongoose.connect("mongodb://localhost/pdog-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  //useFindAndModify: false,
 });
 
 //Template Engine
@@ -31,76 +31,14 @@ app.use(
 
 //Routes
 //önce ulaşmak istediğimiz adresi yazarız ("/")
-app.get("/", async (req, res) => {
-  const photos = await Photo.find({}).sort("-dateCreate");
-  res.render("index", {
-    photos,
-  });
-});
-
-app.get("/photos/:id", async (req, res) => {
-  //console.log(req.params.id);
-  //res.render("about");
-  const photo = await Photo.findById(req.params.id);
-  res.render("photo", {
-    photo,
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/add", (req, res) => {
-  res.render("add");
-});
-
-app.post("/photos", async (req, res) => {
-  //console.log(req.files.image);
-  //res.render("add");
-  //await Photo.create(req.body);
-  //res.redirect("/");
-  const uploadDir = "public/uploads";
-
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-
-  let uploadImage = req.files.image;
-  let uploadPath = __dirname + "/public/uploads/" + uploadImage.name;
-
-  uploadImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: "/uploads/" + uploadImage.name,
-    });
-    res.redirect("/");
-  });
-});
-
-app.get("/photos/edit/:id", async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  res.render("edit", {
-    photo,
-  });
-});
-
-app.put("/photos/:id", async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  photo.title = req.body.title;
-  photo.description = req.body.description;
-  photo.save();
-
-  res.redirect(`/photos/${req.params.id}`);
-});
-
-app.delete("/photos/:id", async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  let deletedImage = __dirname + "/puclic/" + photo.image;
-  fs.unlinkSync(deletedImage);
-  await Photo.findByIdAndRemove(req.params.id);
-  res.redirect("/");
-});
+app.get("/", photoController.getAllPhotos);
+app.get("/photos/:id", photoController.getPhoto);
+app.post("/photos", photoController.createPhoto);
+app.put("/photos/:id", photoController.updatePhoto);
+app.get("/about", pageController.getAboutPage);
+app.get("/add", pageController.getAddPage);
+app.get("/photos/edit/:id", pageController.getEditPage);
+app.delete("/photos/:id", photoController.deletePhoto);
 
 const port = 3000;
 //server ın çalışması için listen metodu yazmamız gerek
